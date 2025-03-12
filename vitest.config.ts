a3f5ac911535bec92efc2e0af89ccc0ef2fbff51
R3call <thenoobyzombie@gmail.com>
2025-03-12 15:46:54 +0100

    fix: jest dom api with vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    // ...
-  },
-})
+	test: {
+		globals: true,
+    setupFiles: "./src/test/setup.ts"
+	},
+});
